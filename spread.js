9a5630c738015e5aa611fcf25445b6f2395eee0f
Denis Maturin <denis1986mr@icloud.com>
2022-10-17 21:02:42 +0300

    git comit

---
+++ spread.js
@@ -0,0 +1,34 @@
+
+
+// const car = {
+//     model : "Audi",
+//     color : "green",
+//     driver : {
+//         name: " Vasay",
+//         age: 18
+//     },
+// }
+
+
+// const carWithPassengers = {...car, model: "BMW",
+// passengers : ["PUPEL"],
+// driver : {
+//     ...car.driver, name : "Ivan", age: 25 
+// }
+
+// }
+// console.log(car)
+// console.log(carWithPassengers)
+
+// module.exports = {
+//     car,
+//     carWithPassengers
+// }
+
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK!');
+  }
+
+  module.exports ={
+    compileAndroidCode
+  }
