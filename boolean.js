f089973d7eba50fa3f5732025abded7947b67b4f
Denis Maturin <denis1986mr@icloud.com>
2022-10-20 19:01:11 +0300

    git commit

---
+++ boolean.js
@@ -0,0 +1,36 @@
+// let x = true  
+// console.log(typeof x)
+
+// console.log( '1' == 1 )
+// console.log( '1' === 1 )
+
+// console.log( 1 + 1 !== 2 ) //вернет false  
+// console.log( 1 + 2 !== 2 ) //вернет true  
+
+// const car = {
+//     model: "BMW",
+//     driver: {
+//         age: 42,
+//         clothes: {
+//             type: "casual",
+//         } ,
+//     },
+// }
+ 
+// console.log(car? "there is no name")
+
+
+// const foo = null ?? 'default string';
+// console.log(foo);
+
+// const baz = 42 ?? 42;
+// console.log(baz);
+
+// const name = 'OleValera'  
+// const userName1 = name ?? 'Defaut user'   
+// console.log(userName1)
+
+
+const otherUser = null  
+const userName2 = otherUser ?? 'Defaut user'   
+console.log(userName2) 
