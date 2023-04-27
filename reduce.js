4c6cffea0dd7c4cdc32ad599f494215d22516f0e
Denys Maturin <55601983+ArmaturaD@users.noreply.github.com>
2023-04-27 13:20:01 +0300

    git commit

--- reduce.js
+++ reduce.js
@@ -1,27 +1,9 @@
-// // reduce
+const fruits = ["apple", "orange", "apple", "pineapple", "orange", "kiwi"];
 
-// const arr = [10, 20, 30, 40, 50]
-
-// const calback =( acc, rec, index, array) => {
-//     console.log (rec, acc)
-//     return rec + acc;
-// }
-
-// const sum = arr.reduce(
-//     calback,
-//     0
-// )
-
-// console.log(sum)
-
-// function firstNonConsecutive(arr) {
-//   const res = arr.find((item, index) => item !== index + arr[0]);
-//   return Number.isInteger ? res : null;
-// }
-
-// console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
-
-function firstNonConsecutive(arr) {
-  return arr.find((element, index) => element - 1 > arr[index - 1]) ?? null;
-}
-console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
+const newArray = fruits.reduce((acc, rec) => {
+  return {
+    ...acc,
+    [rec]: (acc[rec] ?? 0) + 1,
+  };
+}, {});
+console.log(newArray);
