4f76420d684097772d8285ce0be7e65c3948105b
Denys Maturin <55601983+ArmaturaD@users.noreply.github.com>
2023-05-16 20:29:09 +0300

    git commit

--- s.js
+++ s.js
@@ -21,3 +21,30 @@
 // };
 
 // console.log(sumPositive("dd", 8));
+
+const newArray = [
+  "FC Arsenal England",
+  "FC Milan Italy",
+  "FC Dynamo Kyev",
+  "FC BVB Germani",
+  "FC Real Madrid Spain",
+];
+
+function grup(array) {
+  array.forEach((item, index) => {
+    const onlyClubName = item.split(" ")[1];
+    array[index] = onlyClubName;
+    console.log(onlyClubName);
+  });
+  return array;
+}
+//grup(newArray);
+//console.log(newArray);
+
+function grup2(array) {
+  return array.map((item) => item.split(" ")[1]);
+}
+
+console.log(grup2(newArray));
+
+console.log(newArray);
